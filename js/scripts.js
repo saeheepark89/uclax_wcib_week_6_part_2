f9a28d7aa4df16f409fd9e869fd1639f0af26070
SaeHee <SaeHee@rolands-imac.lan>
2020-11-09 15:17:06 -0800

    beverage page

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-console.log('Hello from js/scripts.js!');
+$(document).ready(function() {
+  $("#button").click(function() {
+    displayPics();
+  });
+});
+
+function displayPics(){
+
+  let imagesArray = new Array();
+
+  imagesArray = ['delivery.png', 'taproom.png'];
+
+  let randomNum = Math.floor(Math.random()*imagesArray.length);
+  let img = imagesArray[randomNum];
+
+  $('#myImages').attr('src', 'images/'+img);
+}
